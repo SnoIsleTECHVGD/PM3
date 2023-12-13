34472970a50c6cdab055e10fb89c26655e09c540
Bujnowski <1717567@mukilteo.wednet.edu>
2023-12-13 13:20:19 -0800

    i hate u github

---
+++ Assets/The Game/Scripts/Shooter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletSprite;
+    public float bulletSpeed = 10;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(bulletSprite, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+        }
+    }
+}
