93e9f7455f0ee1ce7abaf9e129903b4998035b70
Bujnowski <1717567@mukilteo.wednet.edu>
2023-12-15 13:27:55 -0800

    j

--- Assets/The Game/Scripts/Shooter.cs
+++ Assets/The Game/Scripts/Shooter.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+
     public Transform bulletSpawnPoint;
     public GameObject bulletSprite;
     public float bulletSpeed = 10;
 
+    void Start()
+    {
+
+    }
+
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             var bullet = Instantiate(bulletSprite, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
