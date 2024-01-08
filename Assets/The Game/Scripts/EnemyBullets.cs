1b360b8718574ea8b7554486a3128fb0ce067a28
codingcomets <1717567@mukilteo.wednet.edu>
2024-01-08 12:23:15 -0800

    i hate u

---
+++ Assets/The Game/Scripts/EnemyBullets.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullets : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb2d;
+    public float force;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb2d.velocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
